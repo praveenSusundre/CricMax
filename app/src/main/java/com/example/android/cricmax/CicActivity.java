95a998a61c82bd6d208e6d357caed82808277850
Abhishek0104 <abhishek.raut.0104@gmail.com>
2017-09-10 15:18:27 +0530

    Initial commit

---
+++ app/src/main/java/com/example/android/cricmax/CicActivity.java
@@ -0,0 +1,13 @@
+package com.example.android.cricmax;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CicActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cic);
+    }
+}
